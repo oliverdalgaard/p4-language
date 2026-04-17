f7d6f5b3d0350d9dbcf41c1b03ac3480dee64aa3
valentino995 <vtrone24@student.aau.dk>
2026-04-17 16:43:27 +0200

    add and sub with int and float
    check sub for negative results?

--- Matilda/src/Interpreter/Interpreter.cs
+++ Matilda/src/Interpreter/Interpreter.cs
@@ -170,28 +170,64 @@ public static class Interpreter
                         {
                             if (v1 is IntVal ai && v2 is IntVal bi)
                             {
-                                return new BoolVal(v1.AsInt() < v2.AsInt());
+                                return new BoolVal(ai.AsInt() < bi.AsInt());
                             }
                             else if (v1 is FloatVal af && v2 is FloatVal bf)
                             {
-                                return new BoolVal(v1.AsFloat() < v2.AsFloat());
+                                return new BoolVal(af.AsFloat() < bf.AsFloat());
                             }
                             else if (v1 is FloatVal af2 && v2 is IntVal bi2)
                             {
-                                return new BoolVal(v1.AsFloat() < v2.AsInt());
+                                return new BoolVal(af2.AsFloat() < bi2.AsInt());
                             }
                             else if (v1 is IntVal ai2 && v2 is FloatVal bf2)
                             {
-                                return new BoolVal(v1.AsInt() < v2.AsFloat());
+                                return new BoolVal(ai2.AsInt() < bf2.AsFloat());
                             }
                             throw new Exception("Type error: '<' supports only numeric types (int/float)");
                         }
 
                     case BinaryOperators.ADD:
-                        return new IntVal(v1.AsInt() + v2.AsInt());
+                        {
+                            if (v1 is IntVal ai && v2 is IntVal bi)
+                            {
+                                return new FloatVal(ai.AsInt() + bi.AsInt());
+                            }
+                            else if (v1 is FloatVal af && v2 is FloatVal bf)
+                            {
+                                return new FloatVal(af.AsFloat() + bf.AsFloat());
+                            }
+                            else if (v1 is FloatVal af2 && v2 is IntVal bi2)
+                            {
+                                return new FloatVal(af2.AsFloat() + bi2.AsInt());
+                            }
+                            else if (v1 is IntVal ai2 && v2 is FloatVal bf2)
+                            {
+                                return new FloatVal(ai2.AsInt() + bf2.AsFloat());
+                            }
+                            throw new Exception("Type error: '+' supports only numeric types (int/float)");
+                        }
 
                     case BinaryOperators.SUB:
-                        return new IntVal(v1.AsInt() - v2.AsInt());
+                        {
+                            if (v1 is IntVal ai && v2 is IntVal bi)
+                            {
+                                return new FloatVal(ai.AsInt() - bi.AsInt());
+                            }
+                            else if (v1 is FloatVal af && v2 is FloatVal bf)
+                            {
+                                return new FloatVal(af.AsFloat() - bf.AsFloat());
+                            }
+                            else if (v1 is FloatVal af2 && v2 is IntVal bi2)
+                            {
+                                return new FloatVal(af2.AsFloat() - bi2.AsInt());
+                            }
+                            else if (v1 is IntVal ai2 && v2 is FloatVal bf2)
+                            {
+                                return new FloatVal(ai2.AsInt() - bf2.AsFloat());
+                            }
+                            throw new Exception("Type error: '-' supports only numeric types (int/float)");
+                        }
 
                     case BinaryOperators.MUL:
                         return new IntVal(v1.AsInt() * v2.AsInt());
