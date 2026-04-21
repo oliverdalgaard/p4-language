a903d3e3baa081c13c95aaa40be7b068929f093d
Melih Kavak <mkavak24@student.aau.dk>
2026-04-21 15:14:53 +0200

    Add unit tests for Expr and Interpreter
    Add and reorganize unit tests for expression AST and interpreter. Rename ExprTest to UnaryOpTests and update namespace to MatildaTests; add tests to verify UnaryOp and BinaryOp property initialization. Add a new Interpreter test (EvalExprPrecedenceCheck) that evaluates an expression (1 + 2 * 3) to verify operator precedence and resulting IntVal (7). Also add required using directives.

--- Matilda.Test/AbstractSyntax/ExprTest.cs
+++ Matilda.Test/AbstractSyntax/ExprTest.cs
@@ -1,14 +1,46 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Matilda;
+using System;
 
-namespace Company.TestProject1;
+namespace MatildaTests;
 
 [TestClass]
-public class ExprTest
+public class UnaryOpTests
 {
     [TestMethod]
     // Start unit tests
-    public void TestMethod1()
+    public void SetUnaryOpProperties()
     {
+        // Arrange
+        var expr = new Ref("x", 1);
 
+        // Act
+        var unaryOp = new UnaryOp(UnaryOperators.NOT, expr, 2);
+
+        // Assert
+        Assert.AreEqual(UnaryOperators.NOT, unaryOp.Op);
+        Assert.AreSame(expr, unaryOp.Expr);
+        Assert.AreEqual(2, unaryOp.LineNumber);
+    }
+}
+
+[TestClass]
+public class BinaryOpTests
+{
+    [TestMethod]
+    public void SetBinaryOpProperties()
+    {
+        // Arrange
+        var exprLeft = new IntV(5,7);
+        var exprRight = new IntV(10,5);
+
+        // Act
+        var binaryOp = new BinaryOp(BinaryOperators.ADD, exprLeft, exprRight, 2);
+
+        // Assert
+        Assert.AreEqual(BinaryOperators.ADD, binaryOp.Op);
+        Assert.AreSame(exprLeft, binaryOp.ExprLeft);
+        Assert.AreSame(exprRight, binaryOp.ExprRight);
+        Assert.AreEqual(2, binaryOp.LineNumber);
     }
 }
---
+++ Matilda.Test/Interpreter/InterpreterTest.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Matilda;
+using System;
+
+namespace MatildaTests;
+
+[TestClass]
+public class EvalExprTests
+{
+    [TestMethod]
+    public void EvalExprPrecedenceCheck()
+  {
+      // Arange
+      Expr expr = new BinaryOp(BinaryOperators.ADD, new IntV(1,1),new BinaryOp(BinaryOperators.MUL, new IntV(2,1), new IntV(3,1),2),1);
+
+      var envV = new EnvV();
+      var envP = new EnvP();
+
+      // Act
+      var reuslt = Interpreter.EvalExpr(expr, envV, envP);
+
+      // Assert
+      Assert.IsInstanceOfType(reuslt, typeof(IntVal));
+      Assert.AreEqual(7, reuslt.AsInt());
+  }
+}
