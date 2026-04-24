a1453475329458f5585a9c6d7d019d458507fc8a
Oliver Dalgaard <oliver@ihd.dk>
2026-04-24 17:14:43 +0200

    Fixed substring

--- Matilda/src/Interpreter/Interpreter.cs
+++ Matilda/src/Interpreter/Interpreter.cs
@@ -139,7 +139,7 @@ public static class Interpreter
             case Read read:
                 List<string[]> rows = new List<string[]>();
                 // Open file with filename "" removed
-                using (TextFieldParser textFieldParser = new TextFieldParser(read.FilePath.Substring(1, read.FilePath.Length - 2)))
+                using (TextFieldParser textFieldParser = new TextFieldParser(read.FilePath))
                 {
                     textFieldParser.TextFieldType = FieldType.Delimited;
                     textFieldParser.SetDelimiters(",");
