6593bdb982f351309ad8c1e6191adaf19777d697
valentino995 <vtrone24@student.aau.dk>
2026-04-27 16:12:17 +0200

    test fix

--- Matilda.Test/TypeChecker/TypeCheckerStmtTTest.cs
+++ Matilda.Test/TypeChecker/TypeCheckerStmtTTest.cs
@@ -18,28 +18,27 @@ public class typeCheckerTests
     {
         // arrange 
         Stmt stmt = new Comp(
-            new Assign("x", new IntV(5, 1), 1), // error
-            new Assign("y", new IntV(10, 2), 2) // error
+            new Assign("x", new IntV(5, 1), -1), // error
+            new Assign("y", new IntV(10, 2), -1) // error
         );
         // act 
         var checker = Run(stmt);
         // assert
         var expected = new List<string>
     {
-        "Line 1: Variable x is not declared.",
-        "Line 2: Variable y is not declared."
+        "Line -1: Variable x is not declared.",
+        "Line -1: Variable y is not declared."
     };
         CollectionAssert.AreEqual(expected, checker.errors);
     }
-    /*[TestMethod]
+    [TestMethod]
     public void CompcheckBothStatements2()
     {
         // arrange
-        Stmt stmt = new Comp(new Declaration(new IntT(), "x", new IntV(5, 1), 1),
-        new Assign("x", new IntV(10, 2), 2));
+        Stmt stmt = new Comp(new Declaration(IntT.Instance, "x", new IntV(5, 1), -1), new Assign("x", new IntV(10, 2), -1));
         // act
         var checker = Run(stmt);
         // assert
         Assert.IsFalse(checker.HasErrors());
-    }*/
+    }
 }
