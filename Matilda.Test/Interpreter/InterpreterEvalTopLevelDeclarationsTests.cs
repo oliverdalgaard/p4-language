667e4da8e3890a211e8df82e4ca7e5f708c5cc2b
Melih Kavak <mkavak24@student.aau.dk>
2026-04-29 14:36:32 +0200

    Unit test updated
    - TopLevelDeclaration
    - InterpreterTopLevelDeclaration
    - Parser
    - InterpreterEvalStmt
    - Stmt

---
+++ Matilda.Test/Interpreter/InterpreterEvalTopLevelDeclarationsTests.cs
@@ -0,0 +1,58 @@
+using Matilda;
+
+namespace MatildaTests;
+
+
+[TestClass]
+public class InterpreterSchemaDeclarationTests
+{
+    // Stmt function declaration binds function in procedure environment
+
+    [TestMethod]
+    public void EvalStmtSchemaDeclarationBindsFunctionInProcedureEnvironment()
+    {
+        // Arrange
+        EnvP envP = new EnvP();
+        EnvS envS = new EnvS();
+
+        SchemaDeclaration schema = new SchemaDeclaration(
+            "Test",
+            new List<Column>(),
+            -1
+        );
+
+        // Act
+        Interpreter.EvalTopLevelDeclarations(new List<TopLevelDeclaration> { schema }, envP, envS);
+
+        // Assert
+        Assert.IsNotNull(envS.TryGet("Test"));
+    }
+}
+
+[TestClass]
+public class InterpreterFunctionDeclarationTests
+{
+    // Stmt function declaration binds function in procedure environment
+
+    [TestMethod]
+    public void EvalStmtFunctionDeclarationBindsFunctionInProcedureEnvironment()
+    {
+        // Arrange
+        EnvP envP = new EnvP();
+        EnvS envS = new EnvS();
+
+        FunctionDeclaration function = new FunctionDeclaration(
+            new IntT(),
+            "Test",
+            new List<Parameter>(),
+            new Return(new IntV(1, -1), -1),
+            -1
+        );
+
+        // Act
+        Interpreter.EvalTopLevelDeclarations(new List<TopLevelDeclaration> { function }, envP, envS);
+
+        // Assert
+        Assert.IsNotNull(envP.TryGet("Test"));
+    }
+}
