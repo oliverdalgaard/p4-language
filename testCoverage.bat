383f8b5ea4f21b514dec7d90d2daa50bd91407b9
Oliver Dalgaard <oliver@ihd.dk>
2026-04-30 19:34:15 +0200

    Exclude files

--- testCoverage.bat
+++ testCoverage.bat
@@ -1,4 +1,4 @@
 @echo off
 cd ./Matilda.Test/
-dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=./TestResults/Coverage/
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=./TestResults/Coverage/ /p:ExcludeByFile=\"**/Parser.cs,**/Scanner.cs,**/Matilda.cs\"
 reportgenerator -reports:./TestResults/Coverage/coverage.cobertura.xml -targetdir:./TestResults/Coverage/Html -reporttypes:Html_Dark
