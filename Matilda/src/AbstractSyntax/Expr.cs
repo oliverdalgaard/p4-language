dff64148cd265232552a80b091a29d4aee155d1a
Melih Kavak <mkavak24@student.aau.dk>
2026-05-11 12:59:42 +0200

    Fixed issues with the setup

--- Matilda/src/AbstractSyntax/Expr.cs
+++ Matilda/src/AbstractSyntax/Expr.cs
@@ -107,23 +107,22 @@ public class Sum : Expr
     }
 }
 
-public class JOIN : Expr
+public class Join : Expr  // Convention: PascalCase, not all caps
 {
     public Expr TableExpr1 { get; }
     public Expr TableExpr2 { get; }
-    public string Column1Id { get; }
-    public string Column2Id { get; }
+    public string PrimaryColumn { get; }
+    public string ForeignColumn { get; }  
     public string ResultSchemaId { get; }
     public override int LineNumber { get; }
 
-    public JOIN(Expr tableExpr1, Expr tableExpr2, string column1, string column2, string resultSchemaId, int lineNumber)
+    public Join(Expr tableExpr1, Expr tableExpr2, string primaryColumn, string foreignColumn, string resultSchemaId, int lineNumber)
     {
         TableExpr1 = tableExpr1;
-        TableExpr2 = tableExpr1;
-        Column1Id = column1;
-        Column2Id = column2;
+        TableExpr2 = tableExpr2;
+        PrimaryColumn = primaryColumn;
+        ForeignColumn = foreignColumn;
         ResultSchemaId = resultSchemaId;
-
         LineNumber = lineNumber;
     }
 }
